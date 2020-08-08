5d337e12df94527d0b8b16dc530f65ad99a0f914
williame <williame_lima@hotmail.com>
2020-08-08 20:40:28 -0300

    page dashBoard

--- src/Routes/RouteBotton.js
+++ src/Routes/RouteBotton.js
@@ -1,18 +1,52 @@
 import React from 'react'
-
+import { Image } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import RouteDashBoard from './RouteDashBoard'
+import ListaEstabelecimentos from '../pages/ListaEstabelecimentos/ListaEstabelecimentos'
 
 const Tab = createBottomTabNavigator(); 
 
 
 export default function RouteBotton() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({route}) => ({
+                tabBarIcon:({color, size}) =>{
+
+                    switch(route.name){
+                        case 'Inicio' : return <Image source={require('../Assets/images/home.png')}
+                                                    style={{width: size, height: size, borderRadius: 10, tintColor: color}}
+                                                />
+                        break;
+                       /*  case 'Busca' : return <Image source={require('../assets/images/estabelecimentos.png')}
+                                                    style={{width: size, height: size, borderRadius: 10, tintColor: color}}
+                                                />
+                        break;
+                        case 'Perfil' : return <Image source={require('../assets/images/carrinho.png')}
+                                                    style={{width: size, height: size, borderRadius: 10, tintColor: color}}
+                                                /> */
+                        break;
+                    }
+
+                }
+            })}
+        >
             <Tab.Screen
-                name='RouteDashBoard'
+                name='Inicio'
                 component={RouteDashBoard}
             />
+            <Tab.Screen
+                name='Estabelecimentos'
+                component={ListaEstabelecimentos}
+            />
+            {/* <Tab.Screen
+                name='Inicio'
+                component={RouteDashBoard}
+            />
+            <Tab.Screen
+                name='Inicio'
+                component={RouteDashBoard}
+            /> */}
         </Tab.Navigator>
     )
 }
---
+++ src/componentes/CarroselOfetas.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function CarroselOfetas() {
+    return (
+        <View>
+            <Text>CarroselOfetas</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
