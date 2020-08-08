a17511bbc797f5807bbc8cbef44268363950ee8c
williame <williame_lima@hotmail.com>
2020-08-08 20:40:38 -0300

    page dashBoard

--- src/Routes/RouteBotton.js
+++ src/Routes/RouteBotton.js
@@ -3,29 +3,36 @@ import { Image } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import RouteDashBoard from './RouteDashBoard'
 import ListaEstabelecimentos from '../pages/ListaEstabelecimentos/ListaEstabelecimentos'
+import CarrinhoCompras from '../pages/CarrinhoCompras/CarrinhoCompras'
+import PerfilUsuario from '../pages/PerfilUsuario/PerfilUsuario'
 
-const Tab = createBottomTabNavigator(); 
-
+const Tab = createBottomTabNavigator();
 
 export default function RouteBotton() {
     return (
         <Tab.Navigator
-            screenOptions={({route}) => ({
-                tabBarIcon:({color, size}) =>{
+            initialRouteName={'RouteDashBoard'}
+            tabBarOptions={{
+                tabStyle: { backgroundColor: '#B32728' },
+                showLabel: false,
 
-                    switch(route.name){
-                        case 'Inicio' : return <Image source={require('../Assets/images/home.png')}
-                                                    style={{width: size, height: size, borderRadius: 10, tintColor: color}}
-                                                />
-                        break;
-                       /*  case 'Busca' : return <Image source={require('../assets/images/estabelecimentos.png')}
-                                                    style={{width: size, height: size, borderRadius: 10, tintColor: color}}
-                                                />
-                        break;
-                        case 'Perfil' : return <Image source={require('../assets/images/carrinho.png')}
-                                                    style={{width: size, height: size, borderRadius: 10, tintColor: color}}
-                                                /> */
-                        break;
+            }}
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+
+                    switch (route.name) {
+                        case 'Inicio': return <Image source={require('../Assets/images/home.png')}
+                            style={{ width: 40, height: 40 }} />
+                            break;
+                        case 'Estabelecimentos': return <Image source={require('../Assets/images/estabelecimentos.png')}
+                            style={{ width: 40, height: 40 }} />
+                            break;
+                        case 'Carrinho': return <Image source={require('../Assets/images/carrinho.png')}
+                            style={{ width: 40, height: 40 }} />
+                            break;
+                        case 'Perfil': return <Image source={require('../Assets/images/perfil.png')}
+                            style={{ width: 40, height: 40 }} />
+                            break;
                     }
 
                 }
@@ -39,14 +46,14 @@ export default function RouteBotton() {
                 name='Estabelecimentos'
                 component={ListaEstabelecimentos}
             />
-            {/* <Tab.Screen
-                name='Inicio'
-                component={RouteDashBoard}
+            <Tab.Screen
+                name='Carrinho'
+                component={CarrinhoCompras}
             />
             <Tab.Screen
-                name='Inicio'
-                component={RouteDashBoard}
-            /> */}
+                name='Perfil'
+                component={PerfilUsuario}
+            />
         </Tab.Navigator>
     )
 }
