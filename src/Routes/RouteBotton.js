0bd94b899f811e49882d4761febce64060171311
williame <williame_lima@hotmail.com>
2020-08-08 23:06:42 -0300

    page dashBoard

--- src/Routes/RouteBotton.js
+++ src/Routes/RouteBotton.js
@@ -34,7 +34,6 @@ export default function RouteBotton() {
                             style={{ width: 40, height: 40 }} />
                             break;
                     }
-
                 }
             })}
         >
--- src/pages/DashBoard.js/Dashboard.js
+++ src/pages/DashBoard.js/Dashboard.js
@@ -1,11 +1,22 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+import CarroselOferts from '../../componentes/CarroselOfetas'
+import CarroselOfetas from '../../componentes/CarroselOfetas';
 
 export default function Dashboard() {
   return (
     <View style={styles.container}>
-      <Text>asdfasdfasdfasdfasdf</Text>
+      <View style={styles.box1}>
+        <CarroselOfetas />
+      </View>
+      <View style={styles.box2}>
+       
+      </View>
+      <View style={styles.box3}>
+       
+      </View>
     </View>
   );
 }
@@ -13,6 +24,22 @@ export default function Dashboard() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff'
   },
+  box1:{
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  box2:{
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  },
+  box3:{
+    flex: 1,
+    width: '100%',
+    height: '100%'
+  }
 });
 
