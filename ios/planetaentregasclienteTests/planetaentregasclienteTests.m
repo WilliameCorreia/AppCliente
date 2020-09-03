00649a61ff557faf28dbd002e00db7a769f4007f
williame <williame_lima@hotmail.com>
2020-09-02 22:56:02 -0300

    mudança de package name do projeto

--- ios/planetaentregasclienteTests/planetaentregasclienteTests.m
+++ ios/planetaentregasclienteTests/planetaentregasclienteTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AppClienteTests : XCTestCase
+@interface planetaentregasclienteTests : XCTestCase
 
 @end
 
-@implementation AppClienteTests
+@implementation planetaentregasclienteTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
